1e1d7103b478c4fcead953f0cc0399961aebe55b
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-06-01 11:01:15 +0200

    placing of dotenv calling change

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+const port = process.env.PORT;
+
+const api_key = process.env.APIKEY;
+
 app.get("/", (req, res) => {
   res.send("Welcome to my node app!");
 });
@@ -32,7 +36,3 @@ app.listen(4242, () => {
 app.use((req, res, next) => {
   res.status(404).send("The page was not found");
 });
-
-const port = process.env.PORT;
-
-const api_key = process.env.APIKEY;
